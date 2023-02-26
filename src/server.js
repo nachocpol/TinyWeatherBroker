26c6ea6904510c33aa90d702727dfe7836a8845a
nacho <nachocpol@gmail.com>
2023-02-26 18:55:17 +0000

    Working on the user login/register infrastructure

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-// Load env variables
-require("dotenv").config()
-
 // Ensure we have the required dependencies
+const dotenv = require("dotenv")
 const express = require("express")
 const mysql = require("mysql")
+const path = require("path")
+
+// Load env variables
+dotenv.config()
 
 const db = mysql.createPool({
     host : process.env.DB_HOST,
@@ -24,8 +26,26 @@ db.getConnection((err, connection) => {
     }
 })
 
+
+const publicAppDir = path.join(__dirname, "./public")
 const appPort = process.env.APP_PORT
 const app = express()
+app.set("view engine", 'hbs')
+app.use(express.static(publicAppDir))
+console.log("Public path for app is: " + publicAppDir)
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
 app.listen(appPort ,() => {
-    console.log('App started. Listening on ${appPort}')
+    console.log('App started')
 })
